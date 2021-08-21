57067cd315d94a26e72b59ac0b6fded3a41eff86
Sathvik S Rao <sathvikrao04@gmail.com>
2021-08-21 23:59:26 +0530

    Backend done

---
+++ backend/data.py
@@ -0,0 +1,135 @@
+import pyrebase
+import csv
+firebaseConfig={'apiKey': "AIzaSyA-s0Bjin-4gmuOUc_PVXRmY0dMHFMk_BE",
+    'authDomain': "hackathon-9bd4b.firebaseapp.com",
+    'databaseURL': "https://hackathon-9bd4b-default-rtdb.firebaseio.com",
+    'projectId': "hackathon-9bd4b",
+    'storageBucket': "hackathon-9bd4b.appspot.com",
+    'messagingSenderId': "57880205401",
+    'appId': "1:57880205401:web:4a75c6e81602ef95573948",
+    'measurementId': "G-ME8ZRNZZGF"
+  }
+firebase=pyrebase.initialize_app(firebaseConfig)
+auth=firebase.auth()
+database=firebase.database()
+def update_student_score(score,streak,classcode,student_id): #to update student scores
+    database.child("Classes").child(classcode).child(student_id).update({"Score":score,"Streak":streak})
+    database.child("Students").child(student_id).child(classcode).update({"Score":score,"Streak":streak})
+        
+
+def add_teacher(teacherid,teacheremail,classnames,teachername):#to add teachers to database
+    database.child("Teachers").child(teacherid).set({"Name":teachername,"Email":teacheremail,"Classes":classnames})
+    for i in classnames:
+        database.child("Classes").push({"Teacher":teachername,"ClassName":i,"Teacherid":teacherid,"Rewards":{"R1":{"Score":100},"R2":{"Score":200},"R3":{"Score":300},"R4":{"Score":400},"R5":{"Score":400}}})
+    a=database.child("Classes").get().val()
+    code={}
+    for i in a:
+        code.update({a[i]["ClassName"]:i})
+    return code # returns code for classnames
+
+def studentdata(studentid,studentname,classcode,studentemail): #adds new student
+    database.child("Students").child(studentid).set({"Name":studentname,"Email":studentemail})
+    database.child("Students").child(studentid).child(classcode).update({"Score":0,"Streak":0})
+    a=database.child("Classes").child(classcode).get().val()
+    
+    a.update({studentid:{"Name":studentname,"Score":0,"Streak":0}})
+    database.child("Classes").child(classcode).set(a)
+    
+def get_classcode(teacherid): # gets classcode for corresponding classnames
+    a=database.child("Classes").get().val()
+    code={}
+    for i in a:
+        if a[i]["Teacherid"]==teacherid:
+            code.update({a[i]["ClassName"]:i})
+    return code
+
+def rewards(classcode):
+    #calling this function after every class ends
+    a=database.child("Classes").get().val()
+    for i in a:
+        if i==classcode:
+            for k in a[i]:
+                if k!="ClassName" and k!="Teacherid" and k!="Teacher" and k!="Rewards":
+                    score=a[i][k]["Score"]
+                    d=a[i]["Rewards"]
+                
+                    for j in d:
+                        
+                        if d[j].get(k)!="Already Redeemed":
+                            if score>=d[j]["Score"]:
+                                a[i]["Rewards"][j].update({k:"Redeemable"})
+                            else:
+                                a[i]["Rewards"][j].update({k:"Not Redeemable"})
+                        
+                        
+    database.child("Classes").update(a)
+            
+            
+def displayrewardstatus(studentid,classname):# get studentid from login['localId']
+    a=database.child("Classes").get().val()
+    for i in a:
+        if i==classcode:
+            for k in a[i]:
+                if k!="ClassName" and k!="Teacherid" and k!="Teacher" and k!="Rewards":
+                    score=a[i][k]["Score"]
+                    d=a[i]["Rewards"]
+                    for j in d:
+                        print(d[j],d[j][studentid])
+                        if d[j][studentid]=="Redeemable":
+                            y=input('Do you wanna redeem it:').lower()
+                            if y=="yes":
+                                a[i]["Rewards"][j].update({studentid:"Already Redeemed"})
+                            
+                            
+                
+                    
+                        
+                        
+    
+                    
+    
+def lastclass():
+    pass
+if __name__=="__main__":
+    choice=int(input("1 or 2 or 3 or 4:"))
+    if choice==1: #adds students 
+        email = input(" Enter Your Email Address : ")
+        password=input('Enter your password: ')
+        confirmpass=input('Confirm Password: ')
+        if password==confirmpass:
+            user = auth.create_user_with_email_and_password(email, password)
+            #auth.send_email_verification(user['idToken']) #sending email verification (if needed)
+            print("Created")
+            print("Enter name:")
+            studentname=input()
+            studentid=user["localId"]
+            studentemail=user["email"]
+            for i in range(int(input("Enter number of subjects"))):
+                print("Enter class code:")
+                classcode=input()
+                studentdata(studentid=studentid,classcode=classcode,studentemail=studentemail,studentname=studentname)
+    elif choice==2: #adds teacher to classes and teachers nodes
+        email = input(" Enter Your Email Address : ")
+        password=input('Enter your password: ')
+        confirmpass=input('Confirm Password: ')
+        if password==confirmpass:
+            user = auth.create_user_with_email_and_password(email, password)
+            #auth.send_email_verification(user['idToken']) #sending email verification (if needed)
+            print("Created")
+            classnames=[]
+            
+            print("Enter name:")
+            name=input()
+            teacherid=user["localId"]
+            teacheremail=user["email"]
+            for i in range(int(input("Enter number of classes:"))):
+                classnames+=[input("Enter class code:")]
+            print(add_teacher(teacherid=teacherid,teacheremail=teacheremail,classnames=classnames,teachername=name))
+                
+    elif choice==3: #--> to get classnames and classcode for teachers
+        email=input("Enter email:")
+        password = input(" Enter Your Password : ")
+        login= auth.sign_in_with_email_and_password(email,password)
+        teacherid=login['localId']
+        classnames=get_classcode(teacherid) #--> dictionary with classname as keys and classcodes as values
+        print(classnames)
---
+++ backend/teacher.py
@@ -0,0 +1,107 @@
+import csv
+import copy
+import pyrebase
+import data
+import os
+firebaseConfig={'apiKey': "AIzaSyA-s0Bjin-4gmuOUc_PVXRmY0dMHFMk_BE",
+    'authDomain': "hackathon-9bd4b.firebaseapp.com",
+    'databaseURL': "https://hackathon-9bd4b-default-rtdb.firebaseio.com",
+    'projectId': "hackathon-9bd4b",
+    'storageBucket': "hackathon-9bd4b.appspot.com",
+    'messagingSenderId': "57880205401",
+    'appId': "1:57880205401:web:4a75c6e81602ef95573948",
+    'measurementId': "G-ME8ZRNZZGF"
+  }
+firebase=pyrebase.initialize_app(firebaseConfig)
+auth=firebase.auth()
+database=firebase.database()
+def bubbleSort(arr,arr1,arr2,arr3):
+    n = len(arr)
+    for i in range(n-1):
+        for j in range(0, n-i-1):
+            if int(arr[j]) > int(arr[j+1]) :
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+                arr1[j],arr1[j+1]=arr1[j+1],arr1[j]
+                arr2[j],arr2[j+1]=arr2[j+1],arr2[j]
+                arr3[j],arr3[j+1]=arr3[j+1],arr3[j]
+    return arr,arr1,arr2,arr3
+                
+            
+        
+def reading(classcode): #collecting data from firebase in a sorted manner
+
+    a=database.child("Classes").child(classcode).get().val()
+    reader=[]
+    
+    for i in a:
+        if i!="ClassName" and i!="Teacher" and i!="Teacherid" and i!="Rewards":
+            
+            reader+=[[a[i]["Name"],a[i]["Score"],a[i]["Streak"],i]]
+    
+    score=[]
+    name=[]
+    streakcalc=[]
+    userid=[]
+    for i in reader:
+            
+        name+=[i[0]]
+        score+=[int(i[1])]
+        streakcalc+=[int(i[2])]
+        userid+=[i[3]]
+
+    score,name,streakcalc,userid=bubbleSort(score,name,streakcalc,userid)
+    return score,name,streakcalc,userid
+        
+    
+  
+def startclass(classcode):
+    i=0
+    score,name,streakcalc,userid=reading(classcode)
+    
+    while True:
+        if i<len(score):
+
+            print(f"{name[i]} has a score of {score[i]}")
+            
+            s=int(input("Enter interaction score:"))
+            bonus=int(input("Enter bonus points:"))
+            if s+bonus>=4:
+                streakcalc[i]+=1
+                
+            else:
+                streakcalc[i]=0
+            if streakcalc[i]!=0 and streakcalc[i]%3==0:
+                score[i]+=5 #the points added due to streak of 3 interactions rated above 4
+            score[i]+=s+bonus
+        else:
+            i=0
+            score,name,streakcalc,userid=bubbleSort(score,name,streakcalc,userid)
+            continue
+            
+        print("Do u wanna end the class:")
+        
+        i+=1
+        if input()=="yes":
+            endclass(score,name,streakcalc,userid,"teacher.csv",classcode)
+            break
+        
+def endclass(score,name,streakcalc,userid,filename,classcode):
+    with open(filename,"w",newline="") as fh:
+        writer=csv.writer(fh)
+        writer.writerow(["Name","Score","Streak"])
+        for i in range(len(score)):
+            writer.writerow([name[i],score[i],streakcalc[i]])
+            data.update_student_score(score[i],streakcalc[i],classcode,userid[i])
+        data.rewards(classcode)
+def openfile(file):
+    os.startfile(file)
+email=input("Enter email:")
+password = input(" Enter Your Password : ")
+login= auth.sign_in_with_email_and_password(email,password)
+teacherid=login['localId']
+classcodes=data.get_classcode(teacherid)
+classname=input("Enter classname:")
+classcode=classcodes[classname]
+startclass(classcode)
+
+openfile("teacher.csv")
